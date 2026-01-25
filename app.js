2d13c52538b2aa795c8436ae0c15cc451bb16410
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2026-01-25 11:51:39 +1100

    Change listener port

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, error => {
     if(error) {
         throw error
